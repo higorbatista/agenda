24963cfe4d2528d64b99de7f2f255fd978d1f996
higorbatista <higor.pm13@gmail.com>
2018-09-13 19:07:21 -0300

    melhorando admin de event

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Event
 # Register your models here.
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("date","event","prioryt")
+    list_display_links = ("event",)
+    list_filter = ("date","prioryt")
+    list_editable = ("prioryt",)
+    search_fields = ("event","date")
+
+
+
